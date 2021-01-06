14a60cc468d8a49fd8d82b37076a7fc13305fb61
jnpdx <john@johnnastos.com>
2021-01-06 15:21:22 -0800

    Drawing audio with SwiftUI

--- RealtimeAudioGraphTest/ContentView.swift
+++ RealtimeAudioGraphTest/ContentView.swift
@@ -6,11 +6,94 @@
 //
 
 import SwiftUI
+import Combine
+
+class DataProvider : ObservableObject {
+    var timer : Timer?
+    
+    var dataPublisher : AnyPublisher<[Float], Never> {
+        return dataSubject
+            .throttle(for: .milliseconds(Int(1.0 / 60.0 * 1000.0)), scheduler: RunLoop.main, latest: false)
+            .eraseToAnyPublisher()
+    }
+    private let dataSubject = CurrentValueSubject<[Float], Never>([0.0])
+    private var generate = false
+    
+    init() {
+        
+    }
+    
+    func startGenerating() {
+        generate = true
+        var floatData = [Float](repeating: 0, count: 1000)
+
+        DispatchQueue.global(qos: .background).async {
+            while self.generate {
+                for index in 0..<floatData.count {
+                    floatData[index] = Float.random(in: 0...1.0)
+                }
+                self.dataSubject.send(floatData)
+                usleep(20_000)
+            }
+        }
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject var dataProvider = DataProvider()
+    @State var valuesToDisplay : [Float] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        VStack {
+//            Text("Hello, world!")
+//            if valueToDisplay.count > 1 {
+//                Text("\(valueToDisplay[0]),\(valueToDisplay[1])")
+//            }
+//        }
+        AudioVisualization(graphData: valuesToDisplay)
+        .padding()
+        .onReceive(dataProvider.dataPublisher) { newValue in
+            valuesToDisplay = newValue
+        }
+        .onAppear {
+            dataProvider.startGenerating()
+        }
+    }
+}
+
+let kSamplesPerPixel : UInt32 = 40
+
+struct AudioVisualization: View {
+    var graphData: [Float]
+    
+    var body: some View {
+        GeometryReader { geometry in
+            
+            //draw a grid every 1000 samples
+            Path { path in
+                let gridColumnWidth = CGFloat(1000) / CGFloat(kSamplesPerPixel)
+                for gridColumnIndex in 0..<Int(geometry.size.width/gridColumnWidth) {
+                    path.move(to: CGPoint(x: CGFloat(gridColumnIndex) * gridColumnWidth, y: 0))
+                    path.addLine(to: CGPoint(x: CGFloat(gridColumnIndex) * gridColumnWidth, y: geometry.size.height))
+                }
+            }
+            .stroke(Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.1))
+            
+            //the waveform
+            Path { path in
+                let pointWidth = geometry.size.width / CGFloat(graphData.count)
+                let halfHeight = geometry.size.height / 2
+                for pointIndex in 0..<graphData.count {
+                    let pointValue = graphData[pointIndex]
+                    let xPos = CGFloat(pointIndex) * pointWidth
+                    let yLength = max(0.5,halfHeight * CGFloat(pointValue))
+                    path.move(to: CGPoint(x: xPos,
+                                          y: halfHeight - yLength))
+                    path.addLine(to: CGPoint(x: xPos, y: halfHeight + yLength))
+                }
+            }
+            .stroke()
+        }.drawingGroup()
     }
 }
 
