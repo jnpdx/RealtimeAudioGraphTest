cfba37b9fced3e240bde253e24dfad2c9b6a008e
jnpdx <john@johnnastos.com>
2021-01-06 22:13:55 -0800

    Can do relatively low-CPU drawing of incoming audio.  No circular buffer yet

--- RealtimeAudioGraphTest/ContentView.swift
+++ RealtimeAudioGraphTest/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Combine
+import AVFoundation
 import os.log
 
 class DataProvider : ObservableObject {
@@ -41,9 +42,69 @@ class DataProvider : ObservableObject {
     }
 }
 
+class AudioInputController : ObservableObject {
+    var audioBuffer : ContiguousArray<Float> = ContiguousArray<Float>(repeating: 0.0, count: 8000)
+    private let dataSubject = CurrentValueSubject<Float, Never>(0.0)
+    var dataPublisher : AnyPublisher<Float, Never> {
+        dataSubject
+            .eraseToAnyPublisher()
+    }
+    var audioEngine = AVAudioEngine()
+    
+    func setupEngine() {
+        setupAudioSession()
+        audioEngine = AVAudioEngine()
+        let recordingFormat = audioEngine.inputNode.inputFormat(forBus: 0)
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (pcmBuffer, timestamp) in
+            //take the previous frames and shift them left
+
+            let insertionIndex = (self.audioBuffer.count - Int(pcmBuffer.frameLength))
+//
+//            var bufferCopy = self.audioBuffer
+//
+//            memcpy(&self.audioBuffer[0], &bufferCopy[Int(pcmBuffer.frameLength)], insertionIndex * MemoryLayout<Float32>.size)
+//
+            //write the new stuff onto the end
+            memcpy(&self.audioBuffer[insertionIndex], pcmBuffer.floatChannelData!.pointee, MemoryLayout<Float32>.size * Int(pcmBuffer.frameLength))
+            //memset(&self.audioBuffer[0], 0, MemoryLayout<Float32>.size)
+            //self.audioBuffer[0] = Float.random(in: 0.0...1.0)
+            
+            self.dataSubject.send(Float.random(in: 0.0...2.0))
+        }
+        audioEngine.prepare()
+    }
+    
+    func setupAudioSession() {
+        #if os(iOS)
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setActive(true)
+            try session.setCategory(
+                .playAndRecord,
+                mode: .default,
+                options: [.defaultToSpeaker])
+        } catch {
+            assertionFailure("Error setting session active: \(error.localizedDescription)")
+        }
+        #endif
+    }
+    
+    func start() {
+        do {
+            try audioEngine.start()
+        } catch {
+            fatalError("Couldn't start engine")
+        }
+    }
+}
+
 struct ContentView: View {
     @ObservedObject var dataProvider = DataProvider()
-    @State var valuesToDisplay : [Float] = []
+    var audioInputController = AudioInputController()
+    //@State var valuesToDisplay : ContiguousArray<Float> = []
+    @State var timestamp : UInt64 = 0
+    
+    @State private var cancelables = Set<AnyCancellable>()
     
     var body: some View {
 //        VStack {
@@ -52,14 +113,24 @@ struct ContentView: View {
 //                Text("\(valueToDisplay[0]),\(valueToDisplay[1])")
 //            }
 //        }
-        AudioVisualization(graphData: valuesToDisplay)
+        AudioVisualization(timestamp: timestamp, audioInputController: audioInputController)
         .padding()
         .onReceive(dataProvider.dataPublisher) { newValue in
-            //os_log("New value: %f",(Date().timeIntervalSince1970))
-            valuesToDisplay = newValue
+            //valuesToDisplay = newValue
         }
         .onAppear {
-            dataProvider.startGenerating()
+            //dataProvider.startGenerating()
+            audioInputController.setupEngine()
+            audioInputController.start()
+            cancelables.insert(
+                audioInputController
+                    .dataPublisher
+                    //.throttle(for: .milliseconds(Int(1.0 / 200.0 * 1000.0)), scheduler: RunLoop.main, latest: false)
+                    .sink { _ in
+                        self.timestamp = mach_absolute_time()
+                        //self.valuesToDisplay = audioInputController.audioBuffer
+                }
+            )
         }
     }
 }
@@ -67,7 +138,8 @@ struct ContentView: View {
 let kSamplesPerPixel : UInt32 = 40
 
 struct AudioVisualization: View {
-    var graphData: [Float]
+    var timestamp : UInt64
+    var audioInputController : AudioInputController
     
     var body: some View {
         GeometryReader { geometry in
@@ -86,10 +158,10 @@ struct AudioVisualization: View {
             
             //the waveform
             Path { path in
-                let pointWidth = geometry.size.width / CGFloat(graphData.count)
+                let pointWidth = geometry.size.width / CGFloat(audioInputController.audioBuffer.count)
                 let halfHeight = geometry.size.height / 2
-                for pointIndex in 0..<graphData.count {//min(5,graphData.count) {
-                    let pointValue = graphData[pointIndex]
+                for pointIndex in 0..<audioInputController.audioBuffer.count {//min(5,graphData.count) {
+                    let pointValue = audioInputController.audioBuffer[pointIndex]
                     let xPos = CGFloat(pointIndex) * pointWidth
                     let yLength = max(0.5,halfHeight * CGFloat(pointValue))
                     path.move(to: CGPoint(x: xPos,
